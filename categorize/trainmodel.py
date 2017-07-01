43522e0cb478281fb465cc6357d9926703f941d0
SilviavanWingerden <silviavw@gmail.com>
2017-07-02 01:30:38 +0200

    Split training from prediction

--- categorize/trainmodel.py
+++ categorize/trainmodel.py
@@ -14,6 +14,7 @@ import re
 
 stemmer = SnowballStemmer("dutch")
 
+
 def getvectorizer():
     vectorizer = CountVectorizer(analyzer="word", tokenizer=None,
                                  preprocessor=None, stop_words=None,
@@ -36,51 +37,29 @@ def task_to_words(raw_task):
     return(" ".join(stemmed))
 
 
-def get_accuracy(test):
-    clean_test_tasks = []
-    for introduction in test["introduction"]:
-        clean_test_tasks.append(task_to_words(introduction))
-    joblib.dump(clean_test_tasks, '/Users/Silvia/Sites/afstuderen/taxonomy-api/joblib/clean_test_tasks.pkl')
+def train_forest(train_data_features, training):
+    forest = BernoulliNB()
+    forest = forest.fit(train_data_features, training["question_type_id"])
+    joblib.dump(forest, '/Users/Silvia/Sites/afstuderen/taxonomy-api/joblib/forest.pkl')
 
+
+def get_train_data_features(training):
+    clean_train_tasks = []
+    for introduction in training["introduction"]:
+        clean_train_tasks.append(task_to_words(introduction))
+    joblib.dump(clean_train_tasks, '/Users/Silvia/Sites/afstuderen/taxonomy-api/joblib/clean_train_tasks.pkl')
+
+    # clean_train_tasks = joblib.load('../joblib/clean_train_tasks.pkl')
     vectorizer = getvectorizer()
+    train_data_features = vectorizer.fit_transform(clean_train_tasks)
+    train_data_features = train_data_features.toarray()
+    joblib.dump(train_data_features, '/Users/Silvia/Sites/afstuderen/taxonomy-api/joblib/train_data_features.pkl')
 
-    test_data_features = vectorizer.transform(clean_test_tasks)
-    test_data_features = test_data_features.toarray()
-    joblib.dump(test_data_features, '/Users/Silvia/Sites/afstuderen/taxonomy-api/joblib/test_data_features.pkl')
-
-    forest = joblib.load('/Users/Silvia/Sites/afstuderen/taxonomy-api/joblib/forest.pkl')
-    result = forest.predict(test_data_features)
-
-    output = pandas.DataFrame(data={"introduction": test["introduction"], "category": result})
-
-    precision = accuracy_score(test['question_type_id'], output['category'])
-    return precision
-
-
-def predict_single(text):
-    array = []
-    array.append(text)
-    single = pandas.DataFrame({'introduction': array})
-
-    clean_task = []
-    for introduction in single["introduction"]:
-        clean_task.append(task_to_words(introduction))
-
-    vectorizer = getvectorizer()
-
-    single_data_feature = vectorizer.transform(clean_task)
-    single_data_feature = single_data_feature.toarray()
-
-    forest = joblib.load('/Users/Silvia/Sites/afstuderen/taxonomy-api/joblib/forest.pkl')
-
-    result = forest.predict(single_data_feature)
-    return str(result[0])
+    train_forest(train_data_features, training)
 
 
 data = pandas.read_csv("/Users/Silvia/Sites/afstuderen/taxonomy-api/data/allquestiontypes.csv", header=0, delimiter=",")
 data = data.dropna(subset=["introduction"])
 training, test = train_test_split(data, test_size=0.2, random_state=12)
 
-# print predict_single("leg uit waarom dit zo is")
-# get_train_data_features(training)
-print get_accuracy(test)
+get_train_data_features(training)
